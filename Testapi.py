b75f4778da94c862ed716be4ed644637e1405a68
boosterBoat <88926888+Epictunarak@users.noreply.github.com>
2026-08-31 13:55:52 +0700

    Update and rename Testapi to Testapi.py

---
+++ Testapi.py
@@ -0,0 +1,26 @@
+import requests
+import os
+
+print("Start Script")
+
+token = os.environ["ONETRUST_TOKEN"]
+
+url = "https://app-apac.onetrust.com/api/template/v1/templates/e1fd8533-b1ef-487c-9e7a-6baf37b8e017/details"
+
+headers = {
+    "Accept": "application/json",
+    "Authorization": f"Bearer {token}"
+}
+
+response = requests.get(
+    url,
+    headers=headers
+)
+
+print("Status Code:", response.status_code)
+print("Content-Type:", response.headers.get("Content-Type"))
+
+with open("response.txt", "w", encoding="utf-8") as f:
+    f.write(response.text)
+
+print("Done")
