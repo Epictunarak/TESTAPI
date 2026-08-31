4e81ce1be47f1e5b66ff7552a14ec236513d1e34
boosterBoat <88926888+Epictunarak@users.noreply.github.com>
2026-08-31 13:59:07 +0700

    Update response handling in getinform.py
    Refactor response handling and save output to response.txt.

--- getinform.py
+++ getinform.py
@@ -6,25 +6,21 @@ print("Start Script")
 token = os.environ["ONETRUST_TOKEN"]
 
 url = "https://app-apac.onetrust.com/api/template/v1/templates/e1fd8533-b1ef-487c-9e7a-6baf37b8e017/details"
-with open("template.json", "w", encoding="utf-8") as f:
-    f.write(response.text)
 
 headers = {
     "Accept": "application/json",
     "Authorization": f"Bearer {token}"
 }
 
-try:
-    response = requests.get(
-        url,
-        headers=headers
-    )
+response = requests.get(
+    url,
+    headers=headers
+)
 
-    print("Status Code:", response.status_code)
-    print("Content Type:", response.headers.get("Content-Type"))
+print("Status Code:", response.status_code)
+print("Content-Type:", response.headers.get("Content-Type"))
 
-    print("Response:")
-    print(response.text[:1000])
+with open("response.txt", "w", encoding="utf-8") as f:
+    f.write(response.text)
 
-except Exception as e:
-    print("ERROR:", str(e))
+print("Done")
