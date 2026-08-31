a3be7a29c42fa93fa80e4486c4bc93158a9fa0b4
boosterBoat <88926888+Epictunarak@users.noreply.github.com>
2026-08-31 13:38:33 +0700

    Enhance getinform.py with error handling
    Added error handling and improved response logging.

--- getinform.py
+++ getinform.py
@@ -1,9 +1,30 @@
 import requests
+import os
+
+print("Start Script")
+
+token = os.environ["ONETRUST_TOKEN"]
 
 url = "https://app-apac.onetrust.com/api/template/v1/templates/e1fd8533-b1ef-487c-9e7a-6baf37b8e017/details"
+with open("template.json", "w", encoding="utf-8") as f:
+    f.write(response.text)
 
-headers = {"accept": "application/json"}
+headers = {
+    "Accept": "application/json",
+    "Authorization": f"Bearer {token}"
+}
 
-response = requests.get(url, headers=headers)
+try:
+    response = requests.get(
+        url,
+        headers=headers
+    )
 
-print(response.text)
+    print("Status Code:", response.status_code)
+    print("Content Type:", response.headers.get("Content-Type"))
+
+    print("Response:")
+    print(response.text[:1000])
+
+except Exception as e:
+    print("ERROR:", str(e))
