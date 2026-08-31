653e96e83462c926bbe822eed26298c360f00def
boosterBoat <88926888+Epictunarak@users.noreply.github.com>
2026-08-31 13:16:46 +0700

    Add authorization header to API request

--- getinform.py
+++ getinform.py
@@ -1,9 +1,19 @@
 import requests
+import os
+
+TOKEN = os.environ["ONETRUST_TOKEN"]
 
 url = "https://app-apac.onetrust.com/api/template/v1/templates/e1fd8533-b1ef-487c-9e7a-6baf37b8e017/details"
 
-headers = {"accept": "application/json"}
+headers = {
+    "Accept": "application/json",
+    "Authorization": f"Bearer {TOKEN}"
+}
 
-response = requests.get(url, headers=headers)
+response = requests.get(
+    url,
+    headers=headers
+)
 
+print("Status:", response.status_code)
 print(response.text)
